c561afe6709eaae42a13c41292769236ae87a75d
wangshuangkc <sw2553@nyu.edu>
2017-08-05 18:36:57 -0700

    First skill-KC Facts

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ var Alexa = require('alexa-sdk');
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = undefined;
 
-var SKILL_NAME = "Space Facts";
+var SKILL_NAME = "KC Facts";
 var GET_FACT_MESSAGE = "Here's your fact: ";
 var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
@@ -19,19 +19,9 @@ var STOP_MESSAGE = "Goodbye!";
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
 var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    'KC is a tennis fan.',
+    'KC is a programmer.',
+    'KC plays League of Legends, as an AD Carry.'
 ];
 
 //=========================================================================================================================================
---
+++ src/lambda-function.js
@@ -0,0 +1,90 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+const languageStrings = {
+    'en': {
+        translation: {
+            FACTS: [
+                'KC is a tennis fan.',
+                'KC is a programmer.',
+                'KC plays League of Legends, as an AD Carry.',
+            ],
+            SKILL_NAME: 'KC Facts',
+            GET_FACT_MESSAGE: "Here's your fact: ",
+            HELP_MESSAGE: 'You can say tell me a KC fact, or, you can say exit... What can I help you with?',
+            HELP_REPROMPT: 'What can I help you with?',
+            STOP_MESSAGE: 'Goodbye!',
+        },
+    },
+    'en-US': {
+        translation: {
+            FACTS: [
+                'KC is a tennis fan.',
+                'KC is a programmer.',
+                'KC plays League of Legends, as an AD Carry.',
+            ],
+        },
+    },
+    'en-GB': {
+        translation: {
+            FACTS: [
+                'KC is a tennis fan.',
+                'KC is a programmer.',
+                'KC plays League of Legends, as an AD Carry.',
+            ],
+        },
+    },
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random KC fact from the KC facts list
+        // Use this.t() to get corresponding language data
+        const factArr = this.t('FACTS');
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+
+        // Create speech output
+        const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
+        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
